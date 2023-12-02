cbc053515b6bdcbf5d3d25d6253febad08706334
admin <gjjeong5325@gmail.com>
2023-12-02 14:32:12 +0900

    pybo app log creation test

--- config/settings/base.py
+++ config/settings/base.py
@@ -206,6 +206,10 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
 
